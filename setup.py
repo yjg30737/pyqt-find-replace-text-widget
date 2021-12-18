94ffc4f1cf36308de45c20ea56d5d950374d8e64
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-18 20:12:14 +0900

    Remove redundant requirements.txt (pyqt-resource-helper is already supposed to be installed by pyqt-find-text-widget)

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     url='https://github.com/yjg30737/pyqt-find-replace-text-widget.git',
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-find-text-widget @ git+https://git@github.com/yjg30737/pyqt-find-text-widget.git@main',
-        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main'
+        'pyqt-find-text-widget @ git+https://git@github.com/yjg30737/pyqt-find-text-widget.git@main'
     ]
 )
