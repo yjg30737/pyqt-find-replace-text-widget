164d3ae20e6d600097edc05cd0c16882581d0add
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 08:35:23 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-find-replace-text-widget',
-    version='0.2.2',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -11,9 +18,11 @@ setup(
                   'pyqt_find_replace_text_widget.ico': ['swap_v.svg', 'close.svg']},
     description='PyQt widget which can find and replace text in the QTextEdit/QTextBrowser',
     url='https://github.com/yjg30737/pyqt-find-replace-text-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-resource-helper>=0.0.1',
-        'pyqt-find-text-widget @ git+https://git@github.com/yjg30737/pyqt-find-text-widget.git@main'
+        'pyqt-find-text-widget>=0.0.1'
     ]
 )
